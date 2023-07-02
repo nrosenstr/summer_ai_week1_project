7eb22f935ec869da1761bc6a6d0309b47730b0e7
nrosenstr <nrosenstrach@gmail.com>
2023-07-02 13:26:01 -0400

    j
    bn

--- social_network_classes.py
+++ social_network_classes.py
@@ -1,38 +1,26 @@
-# A class to hold general system wide social media data and functions. Eg Data objects of all people, Eg functions: Save social media to disk
-class SocialNetwork:
-    def __init__(self):
-        self.list_of_people = [] # this instance variable is initialized to an empty list when social network is created, 
-                                 # you can save objects of people on the network in this list
-        
-    ## For more challenge try this
-    def save_social_media(self):
-        # function to save social media to a file on disk 
-        # hint: look up how to use python's inbuil json module to turn objects to json
-        # you can write this json unto a file on disk
-        pass
 
-    ## For more challenge try this
-    def reload_social_media(self):
-        # function to load saved social media from file on disk 
-        # hint: load a the json file from disk and look up how to recreate the list of people objects.
-        pass
 
-    def  create_account(self):
-        #implement function that creates account here
-        print("Creating ...")
-        pass
 
+account_details = print(input("Please enter a username and password to continue"))
+pass
+# variable that stores users response after creating a new account
 
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, friendlist):
         self.id = name
         self.year = age
-        self.friendlist = []
+        self.friendlist = friendlist
 
     def add_friend(self, person_object):
-        #implement adding friend. Hint add to self.friendlist
+        self.person_object = person_object
         pass
 
     def send_message(self):
-        #implement sending message to friend here
+        # implement sending message to friend here
         pass
+
+
+friendlist = "j"
+currentfriends = []
+
+# You can implement user interface functions here.
